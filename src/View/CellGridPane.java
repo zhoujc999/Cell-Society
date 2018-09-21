66b94ec1c7bd93b7ef484e2cf7a65504ecd79266
Xi Pu <xp19@duke.edu>
2018-09-21 16:08:07 -0400

    testing game of life

--- src/View/CellGridPane.java
+++ src/View/CellGridPane.java
@@ -1,13 +1,16 @@
 package View;
 
+import Model.CellStates;
+import Model.GameOfLifeSimulation;
+import Model.Point;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
-import javafx.util.Duration;
 
-import java.util.Random;
+import java.util.HashMap;
+import java.util.Map;
 
 /*
     @author xp19 This class represents the gridpane that displays cell simulation
@@ -24,31 +27,14 @@ public class CellGridPane {
         initialize(50,50);
     }
 
-    // create a simulation grid
     private void initialize(int width, int height){
-        Rectangle[] rects = new Rectangle[width*height];
 
+        Rectangle[] rects = new Rectangle[width*height];
         for(int i = 0; i < rects.length; i++){
             rects[i] = new Rectangle(CELL_SIZE,CELL_SIZE);
         }
 
-        // testing some random cell simulation
-        Color[] palette = new Color[] { Color.RED, Color.BLUE, Color.ORANGE };
-        Random rng = new Random();
-
-        Timeline tt1 = new Timeline( new KeyFrame(
-                Duration.millis(1000),
-                e -> {
-                    for(int i = 0; i < rects.length; i++){
-                        rects[i].setFill(palette[rng.nextInt(palette.length)]);
-                    }
-                }
-        ));
-        tt1.setCycleCount(Timeline.INDEFINITE);
-        tt1.setAutoReverse(true);
-        tt1.play();
-
-        // add all rectangle objects to the gridpane for displaying
+        Map<Point, CellStates.GameOfLifeStates> map = new HashMap<>();
         int rowIndex = 0;
         int colIndex = 0;
         for(int i = 0; i < rects.length; i++){
@@ -56,8 +42,65 @@ public class CellGridPane {
                 rowIndex += 1;
                 colIndex = 0;
             }
-            gridPane.add(rects[i], colIndex, rowIndex);
+            map.put(new Point(rowIndex, colIndex), CellStates.GameOfLifeStates.DEAD);
             colIndex++;
         }
+
+        // NULL POINTER EXCEPTION HERE
+        GameOfLifeSimulation simulation= new GameOfLifeSimulation(width, height, map);
+        simulation.render();
+//        Map<Point, CellStates.GameOfLifeStates> myMap = simulation.getView();
+
+        int index = 0;
+        for(Point p: map.keySet()){
+
+            if(map.get(p) == CellStates.GameOfLifeStates.LIVE){
+                rects[index].setFill(Color.BLACK);
+            }
+            else{
+                rects[index].setFill(Color.WHITE);
+            }
+            gridPane.add(rects[index++], p.getY(), p.getX());
+        }
+
+        // add time line
     }
+
+
+    // create a simulation grid
+//    private void initialize(int width, int height){
+//        Rectangle[] rects = new Rectangle[width*height];
+//
+//        for(int i = 0; i < rects.length; i++){
+//            rects[i] = new Rectangle(CELL_SIZE,CELL_SIZE);
+//        }
+//
+//        // testing some random cell simulation
+//        Color[] palette = new Color[] { Color.RED, Color.BLUE, Color.ORANGE };
+//        Random rng = new Random();
+//
+//        Timeline tt1 = new Timeline( new KeyFrame(
+//                Duration.millis(1000),
+//                e -> {
+//                    for(int i = 0; i < rects.length; i++){
+//                        rects[i].setFill(palette[rng.nextInt(palette.length)]);
+//                    }
+//                }
+//        ));
+//        tt1.setCycleCount(Timeline.INDEFINITE);
+//        tt1.setAutoReverse(true);
+//        tt1.play();
+//
+//        // add all rectangle objects to the gridpane for displaying
+//        int rowIndex = 0;
+//        int colIndex = 0;
+//        for(int i = 0; i < rects.length; i++){
+//            if(colIndex >= width){
+//                rowIndex += 1;
+//                colIndex = 0;
+//            }
+//            gridPane.add(rects[i], colIndex, rowIndex);
+//            colIndex++;
+//        }
+//    }
 }
